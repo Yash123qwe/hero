8072dabd7b4d5e6399692162a5549f50cdaabbb6
Yash123qwe <75673072+Yash123qwe@users.noreply.github.com>
2021-05-21 00:08:50 +0530

    Add files via upload

---
+++ Hero.js
@@ -0,0 +1,34 @@
+class Hero {
+  constructor(x,y,r)
+	{
+		var options = { 
+			
+			'restitution':0.8,
+            'friction':1.0,
+            'density':1.0,
+            isStatic : true
+			
+		};
+		this.x=x;
+		this.y=y;
+		this.r=r;
+		this.image=loadImage("superhero1.png");
+		this.body=Bodies.circle(this.x, this.y, (this.r)/2, options);
+		World.add(world, this.body);
+
+	}
+	
+	display()
+	{
+			
+			var heroPos=this.body.position;		
+			push()
+			translate(heroPos.x, heroPos.y-100);
+			rectMode(CENTER)
+			fill(255,0,255)
+			imageMode(CENTER);
+			image(this.image, 0,0,this.r+150, this.r)
+			pop()
+			
+	}
+}
